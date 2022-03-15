74200d858a7ab65650dc7e416c5c031c2e812d91
Artur Admayeu <100914706+Artur-Admayeu@users.noreply.github.com>
2022-03-15 13:52:46 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     private final String message = "Hello World!";
 
     public App() {}
+    public EPMA() {while(ture{})}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
