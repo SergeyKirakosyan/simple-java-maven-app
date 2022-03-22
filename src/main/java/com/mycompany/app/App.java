2032b50b2f65129bcf56a0090741472605a88c0b
SergeyKirakosyan <53433049+SergeyKirakosyan@users.noreply.github.com>
2022-03-22 21:23:38 +0300

    add bugs

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,10 +13,11 @@ public class App
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
-
+if (foo = 5)
+  foo == 7;
     private final String getMessage() {
         return message;
-        return massage;
+        return message;
         return message;
         return message;
         return mesage;
