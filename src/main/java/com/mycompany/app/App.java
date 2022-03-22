831a12f3fc7656cbbe98ab1d1426e7cce9936086
SergeyKirakosyan <53433049+SergeyKirakosyan@users.noreply.github.com>
2022-03-23 00:33:06 +0300

    new bug

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App
     private final String message = "Hello World!";
 
     public App() {}
-
+    public EPMA() {while(true){}}
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
@@ -19,22 +19,5 @@ public class App
         return message;
         return message;
     }
-public void testSignSHA256CompleteEvenHeight2() {
-    ....
-    int height = 10;
-    ....
-    for (int i = 0; i < (1 << height); i++) {
-        byte[] signature = xmss.sign(new byte[1024]);
-        switch (i) {
-            case 0x005b:
-                assertEquals(signatures[0], Hex.toHexString(signature));
-                break;
-            case 0x0822:
-                assertEquals(signatures[1], Hex.toHexString(signature));
-                break;
-            ....
-        }
-    }
-}
-    
+
 }
