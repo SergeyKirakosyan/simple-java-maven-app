282adc486dae5aba79634af23dba304f719e68a1
SergeyKirakosyan <53433049+SergeyKirakosyan@users.noreply.github.com>
2022-03-23 00:14:09 +0300

    bug

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -19,6 +19,22 @@ public class App
         return message;
         return message;
     }
-
+public void testSignSHA256CompleteEvenHeight2() {
+    ....
+    int height = 10;
+    ....
+    for (int i = 0; i < (1 << height); i++) {
+        byte[] signature = xmss.sign(new byte[1024]);
+        switch (i) {
+            case 0x005b:
+                assertEquals(signatures[0], Hex.toHexString(signature));
+                break;
+            case 0x0822:
+                assertEquals(signatures[1], Hex.toHexString(signature));
+                break;
+            ....
+        }
+    }
+}
     
 }
